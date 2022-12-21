31ffac8bb3bdab902043075e5c23f2b9222a8395
Andrew <109181049+Andrew-A-Alvarez@users.noreply.github.com>
2022-12-20 16:44:16 -0800

    Delete Mad_lib.py

--- Mad_lib.py
+++
@@ -1,8 +0,0 @@
-person = input("a friends name or a random name either or :")
-verb1 = input("happy verb: ")
-verb2 = input("action verb: ")
-adjective= input("appropriate adjective: ")
-
-madlib = input(f"Once in a far away place {person} was {verb1} with his sister. He then saw a tower and said 'I must {verb2} that place. He was a very {adjective}. The end!!")
-
-print(madlib)
